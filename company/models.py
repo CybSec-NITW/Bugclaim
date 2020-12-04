e50441956e3ee5a0f19c01685627d7815b6f6f69
Aakash074 <aakashkumaar074@gmail.com>
2020-12-04 13:12:35 +0530

    cors

--- company/models.py
+++ company/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    title = models.CharField(max_length = 50)
+    website = models.TextField(max_length = 50)
+    description = models.TextField(max_length = 200)
+    
